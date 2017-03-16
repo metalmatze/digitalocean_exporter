b82e241f5019e1af264b63cb88c4c5f98f21694c
Matthias Loibl <mail@matthiasloibl.com>
2017-03-17 00:30:00 +0100

    Parse configuration from env

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
 	"net/http"
 
+	arg "github.com/alexflint/go-arg"
 	"github.com/digitalocean/godo"
+	"github.com/joho/godotenv"
 	"github.com/metalmatze/digitalocean_exporter/collector"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -14,34 +15,35 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// TokenSource has the AccessToken which is passes to the godo client.
-type TokenSource struct {
-	AccessToken string
+// Config gets its content from env and passes it on to different packages
+type Config struct {
+	DigitalOceanToken string `arg:"env:DIGITALOCEAN_TOKEN"`
+	WebAddr           string `arg:"env:WEB_ADDR"`
+	WebPath           string `arg:"env:WEB_PATH"`
 }
 
 // Token returns a token or an error.
-func (t *TokenSource) Token() (*oauth2.Token, error) {
-	token := &oauth2.Token{
-		AccessToken: t.AccessToken,
-	}
-	return token, nil
+func (c Config) Token() (*oauth2.Token, error) {
+	return &oauth2.Token{AccessToken: c.DigitalOceanToken}, nil
 }
 
 func main() {
-	var (
-		listenAddress = flag.String("web.listen-address", ":9211", "Address on which to expose metrics and web interface.")
-		metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
-		token         = flag.String("do.token", "", "Token you got from DigitalOcean for the API (read-only).")
-	)
-	flag.Parse()
-
-	tokenSource := &TokenSource{
-		AccessToken: *token,
-	}
-	oauthClient := oauth2.NewClient(context.TODO(), tokenSource)
-	client := godo.NewClient(oauthClient)
-
 	log.Println("Starting digitalocean_exporter", version.Info())
+
+	_ = godotenv.Load()
+
+	c := Config{
+		WebPath: "/metrics",
+		WebAddr: ":9211",
+	}
+	arg.MustParse(&c)
+
+	if c.DigitalOceanToken == "" {
+		log.Fatal("DigitalOcean Token is required")
+	}
+
+	oauthClient := oauth2.NewClient(context.TODO(), c)
+	client := godo.NewClient(oauthClient)
 
 	prometheus.MustRegister(collector.NewAccountCollector(client))
 	prometheus.MustRegister(collector.NewDropletCollector(client))
@@ -51,19 +53,19 @@ func main() {
 	prometheus.MustRegister(collector.NewSnapshotCollector(client))
 	prometheus.MustRegister(collector.NewVolumeCollector(client))
 
-	http.Handle(*metricsPath, promhttp.Handler())
+	http.Handle(c.WebPath, promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
+		_, _ = w.Write([]byte(`<html>
 			<head><title>DigitalOcean Exporter</title></head>
 			<body>
 			<h1>DigitalOcean Exporter</h1>
-			<p><a href="` + *metricsPath + `">Metrics</a></p>
+			<p><a href="` + c.WebPath + `">Metrics</a></p>
 			</body>
 			</html>`))
 	})
 
-	log.Println("Listening on", *listenAddress)
-	if err := http.ListenAndServe(*listenAddress, nil); err != nil {
+	log.Println("Listening on", c.WebAddr)
+	if err := http.ListenAndServe(c.WebAddr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
